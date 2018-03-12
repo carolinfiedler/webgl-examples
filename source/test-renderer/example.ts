db78d61016a5634ef4b4ed50affb4c44d8e7bb88
Daniel Limberger <cgdaniel@outlook.com>
2018-03-12 17:21:41 +0100

    support full screen

--- source/test-renderer/example.ts
+++ source/test-renderer/example.ts
@@ -11,6 +11,8 @@ function onload() {
     const renderer = new TestRenderer();
     canvas.renderer = renderer;
 
+    canvas.element.addEventListener('click', function () { gloperate.viewer.Fullscreen.toggle(canvas.element); });
+
     // export variables
     (window as any)['canvas'] = canvas;
     (window as any)['context'] = context;
